809f173b4d304f6b2ce81371ea9fef20adb25c1c
Sophia202407 <sophiawanghongxia@gmail.com>
2025-06-03 22:44:33 +0800

    update: Update App.jsx and other files

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import MemberList from './pages/MemberList';
+import EditMember from './pages/MemberEdit';
+import LoanDueDates from './pages/LoanDueDates';
 import RegisterForm from './pages/RegisterForm';
 import Header from './components/Header';
 import Books from './pages/Books';
 import Loans from './pages/Loans';
 import LoanForm from './components/LoanForm';
 import LoanHistory from './pages/LoanHistory';
-import LoanDueDates from './pages/LoanDueDates';
-
 import './App.css';
+
 const App = () => {
   const [members, setMembers] = useState([]);
 
@@ -26,6 +27,7 @@ const App = () => {
           <Route path="/" element={<Dashboard />} />
           <Route path="/new-loan" element={<LoanForm />} />
           <Route path="/members" element={<MemberList />} />
+          <Route path="/edit-member/:id" element={<EditMember />} />
           <Route path="/books" element={<Books />} />
           <Route path="/loans" element={<Loans />} />
           <Route path="/register" element={<RegisterForm onMemberAdded={handleMemberAdded} />} />
